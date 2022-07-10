24b27933bb04e61617bb3f1e2117f8cfb2dd15ed
MIMIZA <dev@mimiza.com>
2022-07-10 16:05:34 +0700

    update

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Bulk QR code generator in NodeJS
 Example:
 
 ```
-node main.js -u https://google.com/?code="{length:16, option: any option}"
+node main.js -u https://12345.012345.com/qr.html?s="{length: 16}" -n 200 -o "type: svg, margin:1, width: 200"
 ```
--- main.js
+++ main.js
@@ -7,8 +7,14 @@ const dir = "./codes"
 // Check codes folder and create if not exists.
 if (!fs.existsSync(dir)) fs.mkdirSync(dir)
 
-let url = arg.indexOf("-u") > -1 ? arg[arg.indexOf("-u") + 1] : undefined
+const url = arg.indexOf("-u") > -1 ? arg[arg.indexOf("-u") + 1] : undefined
 const number = arg.indexOf("-n") > -1 ? arg[arg.indexOf("-n") + 1] : 100
+const options = { type: "svg", margin: 1, width: 200 }
+if (arg.indexOf("-o") > -1)
+    arg[arg.indexOf("-o") + 1].split(",").forEach(item => {
+        item = item.split(":")
+        options[item[0].trim()] = Number(item[1].trim()) || item[1].trim()
+    })
 
 const generate = ({ length = 16 } = {}) => {
     let code = ""
@@ -31,5 +37,5 @@ for (let i = 1; i <= number; i++) {
         content = content.replace(matches[0], generate(options))
     }
     console.log(`Code #${i}: ${content}`)
-    QRCode.toFile(`${dir}/${i}.svg`, content, { type: "svg" })
+    QRCode.toFile(`${dir}/${i}.${options.type}`, content, options)
 }
