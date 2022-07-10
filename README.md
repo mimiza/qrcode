aeb33992bf62fd89fa9010de8d2576d6a1eef78f
MIMIZA <dev@mimiza.com>
2022-07-10 15:29:58 +0700

    finished

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # qrcode
 
 Bulk QR code generator in NodeJS
+
+Example:
+
+```
+node main.js -u https://google.com/?code="{length:16, option: any option}"
+```
--- main.js
+++ main.js
@@ -7,11 +7,29 @@ const dir = "./codes"
 // Check codes folder and create if not exists.
 if (!fs.existsSync(dir)) fs.mkdirSync(dir)
 
-const url = !isNaN(arg.indexOf("-u")) ? arg[arg.indexOf("-u") + 1] : undefined
-const number = !isNaN(arg.indexOf("-n")) ? arg[arg.indexOf("-n") + 1] : 100
+let url = arg.indexOf("-u") > -1 ? arg[arg.indexOf("-u") + 1] : undefined
+const number = arg.indexOf("-n") > -1 ? arg[arg.indexOf("-n") + 1] : 100
 
 const generate = ({ length = 16 } = {}) => {
     let code = ""
     while (code.length < length) code += Math.floor(Math.random() * 10)
     return code
+}
+
+// Check url for variables.
+const pattern = /{([^{}]+?)}/
+
+for (let i = 1; i <= number; i++) {
+    let matches
+    let content = url
+    while ((matches = pattern.exec(content)) !== null) {
+        const options = {}
+        matches[1].split(",").forEach(item => {
+            item = item.split(":")
+            options[item[0].trim()] = Number(item[1].trim()) || item[1].trim()
+        })
+        content = content.replace(matches[0], generate(options))
+    }
+    console.log(`Code #${i}: ${content}`)
+    QRCode.toFile(`${dir}/${i}.svg`, content, { type: "svg" })
 }
